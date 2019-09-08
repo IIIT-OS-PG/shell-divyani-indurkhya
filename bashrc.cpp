cd3b1fcfb5bb30941bc00d019ee89d9834ae8540
divyani-indurkhya <divyani.indurkhya@students.iiit.ac.in>
2019-09-09 02:50:43 +0530

    history_firstcommit

---
+++ bashrc.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pwd.h>
+using namespace std;
+
+int main()
+{
+  FILE *fp;
+  fp = fopen("bash.txt", "w");
+  // struct passwd *pw = getpwuid(getuid());
+
+  // const char *homedir = pw->pw_dir;
+  // const char *homedir1 = pw->pw_shell;
+  // setenv("USER", pw->pw_name, 1);
+  // printf("%s\n",getenv("USER"));
+  fprintf(fp, "PATH=");
+  fprintf(fp, getenv("PATH"));
+  fprintf(fp, "\n");
+  fprintf(fp, "HOME=");
+  fprintf(fp, getenv("HOME"));
+  fprintf(fp, "\n");
+  fprintf(fp, "USER=");
+  fprintf(fp, getenv("USER"));
+  fprintf(fp, "\n");
+  fprintf(fp, "USERNAME=");
+  fprintf(fp, getenv("USERNAME"));
+  fprintf(fp, "\n");
+  fprintf(fp, "PWD=");
+  fprintf(fp, getenv("PWD"));
+  fprintf(fp, "\n");
+  fprintf(fp, "LOGNAME=");
+  fprintf(fp, getenv("LOGNAME"));
+  fprintf(fp, "\n");
+  fprintf(fp, "PROMPT=$\n");
+  close(fp);
+  
+
+
+  return 0;
+}
